ecab7cb4d98d920e6da3c30140691f5e07b078dd
PazIExplorer <firedestroye.eligriv@gmail.com>
2020-09-15 15:13:15 +0200

    ecran d'accueil
    ajout de l'ecan d'accueil et d'une scene vide

---
+++ Assets/Scripts/changementScene.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class changementScene : MonoBehaviour
+{
+    public string LevelToLoad;
+    public void LoadLevel()
+    {
+        SceneManager.LoadScene(LevelToLoad);
+    }
+}
