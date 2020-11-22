105ac63496865b036ab961086933ad2bd80d268d
PazIExplorer <firedestroye.eligriv@gmail.com>
2020-11-22 16:30:50 +0100

    avancé du jeu
    on avance

--- Assets/Scripts/LevelSelector.cs
+++ Assets/Scripts/LevelSelector.cs
@@ -1,9 +1,37 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 public class LevelSelector : MonoBehaviour
 {
-   public void LoadLevelMatiere(string matiereName)
+    public string valeur;
+    public Button[] matiereButtons;
+
+    private void Start()
     {
-        SceneManager.LoadScene(matiereName);
+        if (valeur != null)
+        {
+            for (int i = 0; i < matiereButtons.Length; i++)
+            {
+                matiereButtons[i].gameObject.SetActive(false);
+                if (matiereButtons[i].gameObject.CompareTag(valeur))
+                {
+                    matiereButtons[i].gameObject.SetActive(true);
+                }
+
+            }
+        }
+        else
+        {
+            for (int i = 0; i < matiereButtons.Length; i++)
+            {
+                matiereButtons[i].gameObject.SetActive(true);
+               
+            }
+        }
+    }
+    public void LoadLevelMatiere(string scene)
+    {
+        SceneManager.LoadScene(scene);
+        
     }
 }
