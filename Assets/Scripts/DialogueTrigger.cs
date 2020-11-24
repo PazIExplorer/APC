19bfccca67c73f27a9813aafea7ecadf67580cf8
PazIExplorer <firedestroye.eligriv@gmail.com>
2020-11-24 11:51:23 +0100

    commit pour pull

---
+++ Assets/Scripts/DialogueTrigger.cs
@@ -0,0 +1,48 @@
+﻿
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class DialogueTrigger : MonoBehaviour
+{
+    
+    public Dialogue dialogue;
+    public Text chatText;
+
+    
+
+    public bool isInRange;
+    // Update is called once per frame
+    void Update()
+    {
+
+        if(isInRange)
+        {
+            TriggerDialogue();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            isInRange = true;
+        }
+
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            isInRange = false;
+        }
+    }
+
+    void TriggerDialogue()
+    {
+        chatText.text = "Voulez vous faire valider:    ";
+        chatText.text += dialogue.chat;
+        chatText.text += " ?";
+    }
+}
