007fca0bdacedbb1d2d03efb7304de120858a1a1
PazIExplorer <firedestroye.eligriv@gmail.com>
2020-11-24 13:19:36 +0100

    il manque la fin

--- Assets/Scripts/DialogueTrigger.cs
+++ Assets/Scripts/DialogueTrigger.cs
@@ -15,29 +15,11 @@ public class DialogueTrigger : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
-        if(isInRange)
-        {
             TriggerDialogue();
-        }
+       
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.CompareTag("Player"))
-        {
-            isInRange = true;
-        }
-
-    }
-
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        if (collision.CompareTag("Player"))
-        {
-            isInRange = false;
-        }
-    }
+    
 
     void TriggerDialogue()
     {
