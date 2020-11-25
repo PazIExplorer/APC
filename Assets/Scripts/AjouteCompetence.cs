0322b9d0d14888949d1195d6047d87acc8d1a95c
FrDarky <32299781+FrDarky@users.noreply.github.com>
2020-11-25 21:52:52 +0100

    Ajout d'une compétence

--- Assets/Scripts/AjouteCompetence.cs
+++ Assets/Scripts/AjouteCompetence.cs
@@ -1,5 +1,8 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using MySql.Data.MySqlClient;
+using MySql.Data.Types;
 
 public class AjouteCompetence : MonoBehaviour
 {
@@ -7,10 +10,59 @@ public class AjouteCompetence : MonoBehaviour
     public InputField cours;
     public InputField competence;
     public InputField description;
-    public void ajouterCompetence()
-    {
+    private MySqlConnection connection;
 
+    private void ConnexionBDD()
+    {
+        if (connection == null)
+        {
+            // INFORMATIONS DE CONNEXION BDD
+            string connectionString = "Host=localhost; Port=3306; Database=apc; Uid=root; Pwd=root;";
+            try
+            {
+                connection = new MySqlConnection(connectionString);
+                connection.Open();
+            }
+            catch (MySqlException ex)
+            {
+                Debug.LogError("MySQL Error: " + ex.ToString());
+            }
+        }
     }
 
-    
+    private void DeconnexionBDD()
+    {
+        if (connection != null)
+        {
+            connection.Close();
+            connection = null;
+        }
+    }
+
+    public void ajouterCompetence()
+    {
+        ConnexionBDD();
+        string commandText = string.Format("INSERT INTO competence SELECT NULL, c.cours_id, '{1}', '{2}' FROM cours c WHERE cours_name LIKE '{0}%'", cours.text, competence.text, description.text);
+        MySqlCommand command = connection.CreateCommand();
+        command.CommandText = commandText;
+        try
+        {
+            int nbLignes = command.ExecuteNonQuery();
+            if (nbLignes > 0)
+            {
+                Debug.Log("Compétence créée avec succès.");
+            }
+            else
+            {
+                Debug.Log("Erreur lors de la création de compétence.");
+            }
+        }
+        catch (System.Exception ex)
+        {
+            // Erreur MySQL
+            Debug.LogError("MySQL error: " + ex.ToString());
+        }
+        DeconnexionBDD();
+        SceneManager.LoadScene("ProfHub");
+    }
 }
