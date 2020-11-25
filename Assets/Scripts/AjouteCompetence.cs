68ef39b1884c2269a2c4618a9365da5f972c0bf6
PazIExplorer <firedestroye.eligriv@gmail.com>
2020-11-25 21:12:46 +0100

    creer competence

---
+++ Assets/Scripts/AjouteCompetence.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AjouteCompetence : MonoBehaviour
+{
+    public Button button;
+    public InputField cours;
+    public InputField competence;
+    public InputField description;
+    public void ajouterCompetence()
+    {
+
+    }
+
+}
